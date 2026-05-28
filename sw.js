0dd419450d194ee82431cb62504d8fd966a54d89
BudgetBook <me@budgetbook.app>
2026-05-28 19:25:05 +0800

    Initial commit: BudgetBook PWA

---
+++ sw.js
@@ -0,0 +1,16 @@
+﻿const CACHE = 'budgetbook-v1';
+const ASSETS = ['/','/index.html','/manifest.json','/icons/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if(resp.ok){ const clone = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); }
+      return resp;
+    }))
+  );
+});
